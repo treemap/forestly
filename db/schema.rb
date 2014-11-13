1861dc328a0dbdbf3ccff6e7d46a91eb08580063
Abhi Yerra <abhi.yerra@lookout.com>
2014-11-13 13:58:23 -0800

    Changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112213100) do
+ActiveRecord::Schema.define(version: 20141112223011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20141112213100) do
     t.integer  "owner_id"
     t.string   "name"
     t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inventories", force: true do |t|
+    t.integer  "property_id"
+    t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
